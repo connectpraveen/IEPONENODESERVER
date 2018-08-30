8f603a66432dc96fe6b7a006863ad3e1dc65d079
connectpraveen <connectpraveen@gmail.com>
2018-08-30 22:11:15 +0530

    commit new code

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 //server.js
 var express=require("express");
 var app=express();
+
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 //routes
 app.get('/',function(req,res){
     res.send('Hello, this server will handle Braintree payments! ');
@@ -19,8 +40,13 @@ gateway.clientToken.generate({
     var clientToken = response.clientToken
   });
 app.get("/client_token", function (req, res) {
+  var Ctoken={"token":""};
+// message
+// {"course":[{"id":3,"information":"test","name":"course1"}],"name":"student"}
     gateway.clientToken.generate({}, function (err, response) {
-      res.send(response.clientToken);
+      //res.send(response.clientToken);
+      Ctoken.token= response.clientToken;
+      res.send(Ctoken);
     });
 });  
 app.post("/checkout", function (req, res) {
@@ -29,7 +55,7 @@ app.post("/checkout", function (req, res) {
 
     gateway.transaction.sale({
         amount: "10.00",
-        paymentMethodNonce: fake-valid-nonce,
+        paymentMethodNonce: nonceFromTheClient,
         options: {
           submitForSettlement: true
         }
